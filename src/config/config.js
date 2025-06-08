ed0b96f2e75247e106bbaf5679e9493f55a35265
Bhavya Popat <bhavyabpopat7777@gmail.com>
2025-06-08 17:05:43 +0530

    removed guild id

--- src/config/config.js
+++ src/config/config.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 module.exports = {
   token: process.env.DISCORD_TOKEN,
   clientId: process.env.CLIENT_ID,
-  guildId: process.env.GUILD_ID,
+  // guildId: process.env.GUILD_ID,
 };
