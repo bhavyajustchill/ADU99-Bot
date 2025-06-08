43e3c356cf22044358a86d96fe3e2bd833d96462
Bhavya Popat <bhavyabpopat7777@gmail.com>
2025-06-08 16:51:45 +0530

    Added KeepAlive

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,10 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    setInterval(() => {
+      console.log(
+        `[KeepAlive] Bot is running at ${new Date().toLocaleTimeString()}`
+      );
+    }, 15000);
   },
 };
